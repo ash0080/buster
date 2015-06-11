f1ec07e8feb2f67a87356cc650df486e8558cb32
圈爷 <ash0080@gmail.com>
2015-06-11 17:29:24 +0800

    quick and dirty fix for Saga theme
    Problem:
    @ Saga using a script.js loading an some JavaScript dynamically
    @ Buster.py is out of date
    
    Fix:
    1. fix the regex for files renaming;
    1. add a cp command to copy the .js files in assets/helper
    2. add 2 regex to remove all ?v=xxxxxxxxx in css and js
    ( maybe it's not necessary but make the html more clear )
    3. just remove the regex of /index.html links change.
    
    Now this folk works fine on linux and mac.
    If your theme contains some dynamic loading js,
    you can simply change the cp path/to/your/folder to make it work.
    
    Signed-off-by: 圈爷 <ash0080@gmail.com>

--- buster/buster.py
+++ buster/buster.py
@@ -36,57 +36,101 @@ def main():
         static_path = arguments['--dir']
     else:
         static_path = os.path.join(os.getcwd(), 'static')
-
+        
     if arguments['generate']:
-        command = ("wget "
+        # quick and dirtyOK cp the links required in .js 
+        getInlineAssets = ("mkdir "
+                    "-p "
+                    "{0}/assets/js "    
+                    "&& "
+                    "cp "
+                    "-rf "
+                    "content/themes/Saga/assets/js/helper "
+                    "{0}/assets/js").format(static_path); 
+
+
+        getWeb = ("wget "
                    "--recursive "             # follow links to download entire site
                    "--convert-links "         # make links relative
                    "--page-requisites "       # grab everything: css / inlined images
                    "--no-parent "             # don't go to parent level
-                   "--directory-prefix {1} "  # download contents to static/ folder
+                   "--directory-prefix={1} "  # download contents to static/ folder
                    "--no-host-directories "   # don't create domain named folder
                    "--restrict-file-name=unix "  # don't escape query string
                    "{0}").format(arguments['--domain'], static_path)
-        os.system(command)
+        os.system(getInlineAssets+' && '+getWeb);
 
-        # remove query string since Ghost 0.4
+        # remove query string since Ghost 0.6.4
         file_regex = re.compile(r'.*?(\?.*)')
+
         for root, dirs, filenames in os.walk(static_path):
             for filename in filenames:
                 if file_regex.match(filename):
-                    newname = re.sub(r'\?.*', '', filename)
-                    print "Rename", filename, "=>", newname
+                    newname = re.sub(r'\?.*', '', filename)  #fix the regex
+                    print newname
                     os.rename(os.path.join(root, filename), os.path.join(root, newname))
 
         # remove superfluous "index.html" from relative hyperlinks found in text
         abs_url_regex = re.compile(r'^(?:[a-z]+:)?//', flags=re.IGNORECASE)
         def fixLinks(text, parser):
             d = PyQuery(bytes(bytearray(text, encoding='utf-8')), parser=parser)
-            for element in d('a'):
+            # for element in d('a'):
+            #     e = PyQuery(element)
+            #     href = e.attr('href')
+            #     print href
+            #     if href is None:
+            #         continue
+            #     new_href = re.sub(r'(rss/index\.html)|(rss/?)$', 'rss/index.rss', href)
+            #     if not abs_url_regex.search(href):
+            #         new_href = re.sub(r'/index\.html$', '/', new_href)
+            #     if href != new_href:
+            #         e.attr('href', new_href)
+            #         print "\t", href, "=>", new_href
+            # remove ?v=XXXXXXXXX in css
+            for element in d('link'):
                 e = PyQuery(element)
                 href = e.attr('href')
-                if not abs_url_regex.search(href):
-                    new_href = re.sub(r'rss/index\.html$', 'rss/index.rss', href)
-                    new_href = re.sub(r'/index\.html$', '/', new_href)
-                    e.attr('href', new_href)
-                    print "\t", href, "=>", new_href
+                if href is None:
+                    continue
+                new_href = re.sub(r'\?.*', '',href)  
+                if href != new_href:
+                    e.attr('href',new_href)
+                    print "\t", href, "=>", new_href     
+            # remove ?v=XXXXXXXXX in js                  
+            for element in d('script'):
+                e = PyQuery(element)
+                src = e.attr('src')
+                if src is None:
+                    continue
+                new_src = re.sub(r'\?.*', '',src) 
+                if src != new_src:
+                    e.attr('src',new_src)
+                    print "\t", src, "=>", new_src
+            ################### 
             if parser == 'html':
                 return d.html(method='html').encode('utf8')
             return d.__unicode__().encode('utf8')
 
-        # fix links in all html files
+
+        # # fix links in all html files
         for root, dirs, filenames in os.walk(static_path):
-            for filename in fnmatch.filter(filenames, "*.html"):
+            for filename in fnmatch.filter(filenames, '*.html'):
                 filepath = os.path.join(root, filename)
+                # print filepath
                 parser = 'html'
-                if root.endswith("/rss"):  # rename rss index.html to index.rss
+                if root.endswith(os.path.sep + 'rss'):  # rename rss index.html to index.rss
                     parser = 'xml'
-                    newfilepath = os.path.join(root, os.path.splitext(filename)[0] + ".rss")
+                    newfilepath = os.path.join(root, os.path.splitext(filename)[0] + '.rss')
+                    try:
+                        os.remove(newfilepath)
+                    except OSError:
+                        pass
                     os.rename(filepath, newfilepath)
                     filepath = newfilepath
                 with open(filepath) as f:
                     filetext = f.read().decode('utf8')
-                print "fixing links in ", filepath
+                    # print filetext
+                # print 'fixing links in ', filepath
                 newtext = fixLinks(filetext, parser)
                 with open(filepath, 'w') as f:
                     f.write(newtext)
